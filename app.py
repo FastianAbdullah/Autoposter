aa7870619f592cd3eada9d4fe25094ad9a9f6671
M.Abdullah <93811691+FastianAbdullah@users.noreply.github.com>
2024-09-15 15:57:52 +0500

    Seperate module for insta and fb autoposting

--- app.py
+++ app.py
@@ -9,10 +9,23 @@ REDIRECT_URI = 'https://autoposter.streamlit.app/'
 # SSL_CERT_PATH = 'cert.pem'
 # SSL_KEY_PATH = 'key.pem'
 
-def login():
+
+st.set_page_config(
+    page_title="Social Media Image Poster",
+    page_icon="📷",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+
+def facebook_login():
     permissions = "pages_read_engagement,pages_manage_posts,instagram_basic,instagram_content_publish"
     auth_url = f"https://www.facebook.com/v20.0/dialog/oauth?client_id={APP_ID}&redirect_uri={REDIRECT_URI}&scope={permissions}"
     st.markdown(f"[Login with Facebook]({auth_url})")
+
+def instagram_login():
+    permissions = "instagram_basic,instagram_content_publish"
+    auth_url = f"https://www.facebook.com/v20.0/dialog/oauth?client_id={APP_ID}&redirect_uri={REDIRECT_URI}&scope={permissions}"
+    st.markdown(f"[Login with Instagram]({auth_url})")
 
 def get_access_token(code):
     token_url = f"https://graph.facebook.com/v20.0/oauth/access_token?client_id={APP_ID}&redirect_uri={REDIRECT_URI}&client_secret={APP_SECRET}&code={code}"
@@ -24,11 +37,20 @@ def get_user_pages(access_token):
     pages_response = requests.get(pages_url)
     return pages_response.json().get('data', [])
 
-def get_instagram_account(page_id, page_token):
-    ig_account_url = f"https://graph.facebook.com/v20.0/{page_id}?fields=instagram_business_account&access_token={page_token}"
-    ig_response = requests.get(ig_account_url)
-    ig_data = ig_response.json()
-    return ig_data.get('instagram_business_account', {}).get('id')
+def get_instagram_accounts(access_token):
+    ig_accounts_url = f"https://graph.facebook.com/v20.0/me/accounts?fields=instagram_business_account{{id,name,username}}&access_token={access_token}"
+    ig_response = requests.get(ig_accounts_url)
+    ig_data = ig_response.json().get('data', [])
+    accounts = []
+    for account in ig_data:
+        if 'instagram_business_account' in account:
+            ig_account = account['instagram_business_account']
+            accounts.append({
+                'id': ig_account['id'],
+                'name': ig_account.get('name', 'Unknown'),
+                'username': ig_account.get('username', 'Unknown')
+            })
+    return accounts
 
 def post_to_facebook(page_id, page_token, image_url, message):
     post_url = f'https://graph.facebook.com/v20.0/{page_id}/photos'
@@ -73,38 +95,65 @@ def main():
     if st.session_state.access_token:
         st.success("You are logged in!")
         st.write(f"Access token: {st.session_state.access_token[:10]}...")
-        st.write(f"Page ID: {st.session_state.page_id}")
-        st.write(f"Instagram User ID: {st.session_state.ig_user_id}")
-
-        image_url = st.text_input("Image URL")
-        message = st.text_input("Message for Facebook")
-        caption = st.text_input("Caption for Instagram")
-
-        if st.button("Post Image"):
-            if image_url and message and caption:
-                fb_result = post_to_facebook(st.session_state.page_id, st.session_state.page_token, image_url, message)
-                ig_result = post_to_instagram(st.session_state.ig_user_id, st.session_state.page_token, image_url, caption)
-                st.write("Facebook result:", fb_result)
-                st.write("Instagram result:", ig_result)
+        
+        col1, col2 = st.columns(2)
+        
+        with col1:
+            st.subheader("Facebook Posting")
+            pages = get_user_pages(st.session_state.access_token)
+            if pages:
+                selected_page = st.selectbox("Select Facebook Page", [f"{page['name']} ({page['id']})" for page in pages])
+                selected_page_id = selected_page.split('(')[-1].strip(')')
+                st.session_state.page_id = selected_page_id
+                st.session_state.page_token = next(page['access_token'] for page in pages if page['id'] == selected_page_id)
+                
+                image_url = st.text_input("Image URL for Facebook")
+                message = st.text_input("Message for Facebook")
+                
+                if st.button("Post to Facebook"):
+                    if image_url and message:
+                        result = post_to_facebook(st.session_state.page_id, st.session_state.page_token, image_url, message)
+                        st.write("Facebook result:", result)
+                    else:
+                        st.warning("Please fill in all Facebook fields")
             else:
-                st.warning("Please fill in all fields")
+                st.warning("No Facebook Pages found. Make sure you have a Facebook Page associated with your account.")
+        
+        with col2:
+            st.subheader("Instagram Posting")
+            ig_accounts = get_instagram_accounts(st.session_state.access_token)
+            if ig_accounts:
+                selected_account = st.selectbox("Select Instagram Account", [f"{account['name']} (@{account['username']})" for account in ig_accounts])
+                selected_account_id = next(account['id'] for account in ig_accounts if f"{account['name']} (@{account['username']})" == selected_account)
+                st.session_state.ig_user_id = selected_account_id
+                
+                ig_image_url = st.text_input("Image URL for Instagram")
+                ig_caption = st.text_input("Caption for Instagram")
+                
+                if st.button("Post to Instagram"):
+                    if ig_image_url and ig_caption:
+                        result = post_to_instagram(st.session_state.ig_user_id, st.session_state.access_token, ig_image_url, ig_caption)
+                        st.write("Instagram result:", result)
+                    else:
+                        st.warning("Please fill in all Instagram fields")
+            else:
+                st.warning("No Instagram Business accounts found. Make sure you have connected an Instagram Business account to your Facebook account.")
     else:
-        login()
+        st.write("Please log in to post to Facebook or Instagram.")
+        col1, col2 = st.columns(2)
+        with col1:
+            facebook_login()
+        with col2:
+            instagram_login()
+        
         code = st.query_params.get("code")
         if code:
             access_token = get_access_token(code)
             if access_token:
                 st.session_state.access_token = access_token
-                pages = get_user_pages(access_token)
-                if pages:
-                    st.session_state.page_token = pages[0]['access_token']
-                    st.session_state.page_id = pages[0]['id']
-                    st.session_state.ig_user_id = get_instagram_account(pages[0]['id'], pages[0]['access_token'])
-                    st.rerun()
-                else:
-                    st.error("No pages found. Make sure you have a Facebook Page.")
+                st.rerun()
             else:
                 st.error("Failed to get access token.")
 
 if __name__ == "__main__":
-  main()
+    main()
